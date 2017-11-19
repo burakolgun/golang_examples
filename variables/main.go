74a2ea0e22724f07249e3ac3a3b932b5789d19aa
Burak Olgun <burak.olgun@kolayik.com>
2017-11-19 17:15:52 +0300

    Go variables types added

---
+++ variables/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+func main() {
+	var variableTypesForInt string = "int, int8, int16, int32, int64"
+	var variableTypesForUint string = "uint, uint8, uint16, uint32, uint64"
+	var boolTrue = true
+	var boolFalse = false
+	var float float64 = 12.442131
+	
+	fmt.Println(variableTypesForInt);
+	fmt.Println(variableTypesForUint)
+	fmt.Println("boolean", boolFalse, boolTrue)
+	fmt.Println("float", float)
+
+}

49f20b866cdef7638dd116d6e7495e4c171cc475
Burak Olgun <burak.olgun@kolayik.com>
2017-11-26 15:00:05 +0300

    Pointer added

---
+++ pointer/pointer.go
@@ -0,0 +1,24 @@
+package main
+
+import("fmt")
+func point(p *int) {
+	*p = 99
+}
+func main() {
+	a:= 5;
+	fmt.Println("a=", a)
+	fmt.Println("a's pointer value=", &a)
+	b := &a
+	fmt.Println("b's value now=", *b)
+	*b = 99
+	fmt.Println("b's value now=", *b)
+	fmt.Println("a's  value now=", a)	
+
+	fmt.Println()
+	fmt.Println()
+	p:= 1
+	fmt.Println("p =", p)	
+	point(&p)
+	fmt.Println("p =", p)
+
+}

babe48af9aa3a3a4c05aca26dc05ca8d3a56c4f1
Burak Olgun <burak.olgun@kolayik.com>
2017-11-26 14:38:47 +0300

    Recursion euclid function added

---
+++ functions/euclid.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func euclid(x,y int) int {
+	if y == 0 {
+		return x
+	}
+
+	return euclid(y, x%y)
+}
+
+func main() {
+	fmt.Println("euclid for 48 and 30 =", euclid(48,30))
+}

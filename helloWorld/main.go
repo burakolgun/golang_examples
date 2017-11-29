fe88eaaabdb965ecfec0e33add8fb1773ccf7712
Burak Olgun <burak.olgun@kolayik.com>
2017-11-29 09:16:17 +0300

    First import and git ignore file added

--- helloWorld/main.go
+++ helloWorld/main.go
@@ -1,7 +1,9 @@
 package main
 import (
 	"fmt"
+	"../import_package"
 )
 func main(){
 	fmt.Println("hello, World")
+	import_package.Now()
 }
---
+++ import_package/import.go
@@ -0,0 +1,8 @@
+package import_package
+
+import "fmt"
+import "time"
+
+func Now() {
+	fmt.Println(time.Now())
+}

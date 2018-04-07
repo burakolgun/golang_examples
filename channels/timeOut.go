f4bec894c5d90f18520c396d972f22d6912316c7
Burak Olgun <burakolgun91@gmail.com>
2018-04-07 11:34:04 +0300

    Add time out sample for channel

---
+++ channels/timeOut.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var chan1 = make (chan bool, 1)
+var chan2 = make (chan bool, 1)
+
+func main() {
+	fmt.Println("Process Starting...")
+	go worker(5)
+	go timer(6)
+
+	select {
+		case <- chan1:
+			fmt.Println("Mission Completed")
+		case <- chan2:
+			fmt.Println("Time Out")
+	}
+}
+
+func timer(second time.Duration) {
+	time.Sleep(second * time.Second)
+	chan2 <- true
+}
+
+func worker (second time.Duration) {
+	fmt.Println("Process Started")
+	time.Sleep(second * time.Second)
+	chan1 <- true
+}

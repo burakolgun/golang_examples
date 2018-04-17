9e6e4a5004ac64d3c3240be88b846a0a9d069719
Burak Olgun <burakolgun91@gmail.com>
2018-04-17 21:05:11 +0300

    Add post request sample

---
+++ net/postRequest.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+func main() {
+	baseUrl := "http://localhost:4000/api/recover"
+
+	form := url.Values{
+		"name": {"burak"},
+		"surname":  {"olgun"},
+		"postRequest":  {"Hello, World"},
+		"from":     {"go"},
+	}
+
+	body := bytes.NewBufferString(form.Encode())
+	rsp, err := http.Post(baseUrl, "application/x-www-form-urlencoded", body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer rsp.Body.Close()
+
+	bodyByte, err := ioutil.ReadAll(rsp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(bodyByte))
+}

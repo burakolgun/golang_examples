3e9cf21fcd52ce7ddc39852bce18bc2416510fb5
Burak Olgun <burak.olgun@trendyol.com>
2019-05-12 22:49:29 +0300

    Add example for json

---
+++ json/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func main() {
+	type Feed struct {
+		UserId    int    `json:"userId"`
+		Id        int    `json:"id"`
+		Title     string `json:"title"`
+		Completed bool   `json:"completed"`
+	}
+
+	const dataFile = "./toDos.json"
+
+	file, err := os.Open(dataFile)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer file.Close()
+
+	var feeds []*Feed
+
+	err = json.NewDecoder(file).Decode(&feeds)
+
+	for index, feed := range feeds {
+		fmt.Println(index, "index")
+		fmt.Println(feed.Completed, " is completed?")
+		fmt.Println(feed.Id, " id")
+		fmt.Println(feed.Title, " title")
+		fmt.Println(feed.UserId, " userId")
+	}
+}

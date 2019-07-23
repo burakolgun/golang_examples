f1af75b9d9a61a13cff9a55659cbf41c46456a01
Burak Olgun <burakolgun91@gmail.com>
2019-07-23 13:47:06 +0300

    Add test dir

---
+++ test/benchmark/fib_test.go
@@ -0,0 +1,22 @@
+package benchmark
+
+import (
+	"fmt"
+	"testing"
+)
+
+func BenchmarkFib10(b *testing.B) {
+	// run the Fib function b.N times
+	for n := 0; n < b.N; n++ {
+		Fib(10)
+		fmt.Println(b.N)
+		fmt.Println(n)
+	}
+}
+
+func Fib(n int) int {
+	if n < 2 {
+		return n
+	}
+	return Fib(n-1) + Fib(n-2)
+}
---
+++ test/benchmark/hello_world_test.go
@@ -0,0 +1,12 @@
+package benchmark
+
+import (
+	"fmt"
+	"testing"
+)
+
+func BenchmarkHello(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fmt.Sprintf("hello, World")
+	}
+}
